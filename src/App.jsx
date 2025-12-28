0ee93da815dddd66d3346a2ca7218056d599b5a4
Alexandros1992 <alex.tsieris@hotmail.com>
2025-12-28 09:31:44 +0200

    Initialisation of the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function MyButton() {
+  return (
+    <button>Click Me!</button>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+export default App
